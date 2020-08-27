2e53ebf11c4bb95a3e64a9693fa55313ef5438f8
gogo-code <3059349417@qq.com>
2020-08-27 10:52:55 +0800

    登录模块完成

--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import './assets/css/global.css'
+
+import axios from 'axios'
+
+axios.defaults.baseURL='http://127.0.0.1:8888/api/private/v1/'
+// 将axios挂载到Vue原型中使得每个组件都可以发起axios请求
+Vue.prototype.$http=axios
+
+
 
 Vue.config.productionTip = false
-
+Vue.use(ElementUI);
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
---
+++ src/views/Login.vue
@@ -0,0 +1,128 @@
+<template>
+  <div class="login_container">
+    <div class="login_box">
+      <div class="avatar_box">
+        <img src="~assets/logo.png" alt />
+      </div>
+      <!-- 表单 -->
+      <el-form
+        ref="loginFormRef"
+        :model="loginForm"
+        :rules="loginFormRules"
+        label-width="0px"
+        class="login_form"
+      >
+        <!-- 用户名 -->
+        <el-form-item prop="username">
+          <el-input v-model="loginForm.username" prefix-icon="el-icon-user-solid"></el-input>
+        </el-form-item>
+        <!-- 密码 -->
+        <el-form-item prop="password">
+          <el-input v-model="loginForm.password" prefix-icon="el-icon-s-goods" type="password"></el-input>
+        </el-form-item>
+        <!-- 按钮 -->
+        <el-form-item class="btns">
+          <el-button type="primary" @click="login">登录</el-button>
+          <el-button type="primary" @click="resetLoginForm">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      // 登录表单数据绑定对象
+      loginForm: {
+        username: "admin",
+        password: "123456",
+      },
+      // 表单验证规则对象
+      loginFormRules: {
+        username: [
+          { required: true, message: "请输入登录名称", trigger: "blur" },
+          { min: 3, max: 10, message: "长度在3到10个字符", trigger: "blur" },
+        ],
+        password: [
+          { required: true, message: "请输入登录密码", trigger: "blur" },
+          {
+            min: 6,
+            max: 15,
+            message: "长度在 6 到 15 个字符",
+            trigger: "blur",
+          },
+        ],
+      },
+    };
+  },
+  methods: {
+    resetLoginForm() {
+      this.$refs.loginFormRef.resetFields();
+    },
+    login() {
+      this.$refs.loginFormRef.validate(async valid => {
+        // 表单预验证
+        if (!valid) return;
+        //解构赋值，await将promise对象转化为数据对象,asyn表示异步操作
+        const { data: res } = await this.$http.post("login", this.loginForm); 
+        
+        if(res.meta.status!==200)
+        return this.$message.error('登录失败')
+        this.$message.success('登录成功')
+        // 登录成功后由于后续api接口需要一些权限，所以需要身份验证token，token只在当前网站打开期间生效，所以保存到客户端的sessionStorage中
+        window.sessionStorage.setItem("token",res.data.token)
+        // 通过编程式导航跳转到后台主页，路由地址为/home
+        this.$router.push("/home")
+      });
+    },
+  },
+};
+</script>
+
+<style lang="less" scoped>
+.login_container {
+  background-color: #2b4b6b;
+  height: 100%;
+}
+.login_box {
+  width: 450px;
+  height: 300px;
+  background-color: #fff;
+  border-radius: 3px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  .avatar_box {
+    height: 130px;
+    width: 130px;
+    border: 1px solid #eee;
+    border-radius: 50%;
+    padding: 10px;
+    box-shadow: 0 0 10px #ddd;
+    position: absolute;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-color: #fff;
+    img {
+      width: 100%;
+      height: 100%;
+      border-radius: 50%;
+      background-color: #eee;
+    }
+  }
+}
+.login_form {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  padding: 0 20px;
+  box-sizing: border-box;
+}
+.btns {
+  display: flex;
+  justify-content: flex-end;
+}
+</style>
